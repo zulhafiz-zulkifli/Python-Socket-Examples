404f83cca6391421b0041f84d741652bbe468552
zulhafiz-zulkifli <zulh2605@gmail.com>
2023-01-10 12:19:24 +0800

    display bytes

--- tcp_client.py
+++ tcp_client.py
@@ -3,7 +3,7 @@
 import socket
 
 # The client must have the same server specifications
-host = '127.0.0.1'
+host = '192.168.196.135'
 port = 12345
 BUFFER_SIZE = 1024
 
@@ -14,4 +14,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
     # We convert str to bytes
     socket_tcp.send(MESSAGE.encode('utf-8'))
     data = socket_tcp.recv(BUFFER_SIZE)
-    print('[*] Data received: {}'.format(data.decode('utf-8'))) 
+    print('[*] Data received: {}'.format(data.decode('utf-8')))
+    print('[*] Data received in bytes: {}'.format(":".join("{:02x}".format(ord(c)) for c in data.decode('utf-8')))) 
